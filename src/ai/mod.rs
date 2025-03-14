aec9763dc2481c17e695b49bd00969ed022eba48
BudzioT <budzio.zsm1@gmail.com>
2025-03-14 11:08:14 +0100

    Basic history!

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,16 +1,23 @@
 use reqwest::Client;
 use serde_json::{json, Value};
-
+use serde::{Deserialize, Serialize};
 
 // The main source of power!
+#[derive(Debug)]
 pub struct Ai {
     groq_key: String,
+    chat_history: Vec<Message>,
 }
 
 impl Ai {
     pub fn new(groq_key: String) -> Ai {
+        let starting_history: Message = Message {
+            role: "system".to_string(),
+            content: "You are a helpful assistant. You are highly specialized in cybersecurity and are ready to help.".to_string(),
+        };
         Ai {
             groq_key,
+            chat_history: vec![starting_history],
         }
     }
 
@@ -19,26 +26,23 @@ impl Ai {
         // Some consts for nice look
         const URL: &str = "https://api.groq.com/openai/v1/chat/completions";
         const MODEL: &str = "llama3-70b-8192";
+        let msg: Message = Message {
+            role: "user".to_string(),
+            content: prompt,
+        };
+        self.chat_history.push(msg);
+
         let body: String = json!({
             "model": MODEL,
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "You are an helpful AI assistant who knows everything about cyber security"
-                },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
+            "messages": self.chat_history
         }).to_string();
 
         // Just request kindly the answer, I love chains of dots
         let response = Client::new()
-            .get(URL)
+            .post(URL)
             .header("Content-Type", "application/json")
             .header("Authorization", format!("Bearer {}", self.groq_key))
-            .body(body.to_string())
+            .body(body)
             .send()
             .await;
         if response.is_err() {
@@ -50,8 +54,24 @@ impl Ai {
         let json: Value = serde_json::from_str(&response_text).unwrap();
         let content = json["choices"][0]["message"]["content"].as_str();
         match content {
-            Some(answer) => answer.to_string(),
+            Some(answer) => {
+                let answer_str = answer.to_string();
+                let answer_msg: Message = Message {
+                    role: "assistant".to_string(),
+                    content: answer_str,
+                };
+                self.chat_history.push(answer_msg);
+
+                answer.to_string()
+            },
             None => "Couldn't generate message, try again later".to_string(),
         }
     }
+}
+
+// Simple message, role - either user or
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Message {
+    pub role: String,
+    pub content: String,
 }
